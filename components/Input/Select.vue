a1b9b84a11453772ba78ba7f103685a155aa1544
slovenianGooner <lovro.papez@gmail.com>
2021-11-14 14:28:44 +0100

    Update Select.vue

--- components/Input/Select.vue
+++ components/Input/Select.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative flex-1">
     <select
-      class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
+      class="appearance-none block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
       :class="[
         errors.length
           ? 'border-red-300 text-red-900 placeholder-red-300 focus:ring-red-300'
